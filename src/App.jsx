219b5e5f7bcab5597ba978748af46cc06089f8aa
Luis0251 <luis0251@hotmail.com>
2023-05-13 12:47:10 -0500

    feat:adding add task functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { CustomForm } from "./components/CustomForm"
 
 function App() {
+  const [tasks,setTasks] = useState([])
+  const addTask = (task) =>{
+    setTasks(prevState =>[...prevState, task])
+  }
   return (
     <div className="mx-4vw max-w-screen-lg sm:mx-auto">
      <div className="grid gap-y-8 md:gap-y-[3.5vmax] text-center">
       <header><h1 className="text-3xl text-center ">My Task List</h1></header>
-      <CustomForm />
+      <CustomForm addTask={addTask} />
      </div>
      </div>
   )
--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -1,25 +1,37 @@
-
-export const CustomForm = () => {
+import {PlusIcon} from '@heroicons/react/24/solid'
+import { useState } from 'react';
+// eslint-disable-next-line react/prop-types
+export const CustomForm = ({addTask}) => {
+   
+    const [task, setTask] = useState("")
     const handleSubmit = (e) =>{
         e.preventDefault();
-        console.log(e);
+        addTask({
+            name:task,
+            checked:false,
+            id:Date.now()
+        });
+        setTask("");
     }
   return (
     <form className="flex gap-2 text-base text-left" onSubmit={handleSubmit}>
-        <div className="flex-1 relative grid gap-2">
+        <div className="flex gap-2">
+            <div className='flex flex-col'>
             <label htmlFor="" className="block mb-2 text-sm font-medium text-white ">Typing</label>
             <input type="text"
             id="task"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5 "
-            //onInput={(e)=> setTask {e.target.value}}
+            value={task}
+            onInput={(e)=> setTask(e.target.value)}
             required
             autoFocus
             maxLength={60} 
             placeholder="Enter task"
              />
-             <button className="flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium  rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500     " 
+             </div>
+             <button className="flex items-center justify-center p-0.5 mb-2  text-sm font-medium  rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500     " 
              aria-label="add task"
-              type="submit">Search</button>
+              type="submit"><PlusIcon  className='h-6 w-6 text-white'/></button>
         </div>
     </form>
   )
